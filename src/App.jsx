b62c4ddc4076143b676f03086b71536a21118d4b
ParasUniyal <parasuniyal2002@gmail.com>
2024-09-27 10:02:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Weather from './Weather'
+import './App.css'
+import './Weather.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+        <Weather></Weather>
+        
+    </>
+  )
+}
+
+export default App
